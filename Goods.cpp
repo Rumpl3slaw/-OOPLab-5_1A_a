e1225c1e1acbbd83298ae2867a2da4231ab63d44
Rumpl3slaw <wowpanda13@ukr.net>
2021-05-20 17:48:05 +0300

    zxc

---
+++ Goods.cpp
@@ -0,0 +1,112 @@
+﻿#include <sstream>
+#include <Windows.h>
+
+#include "Goods.h"
+#include "My_Error_Range.h"
+
+
+Goods::Goods()
+{
+	first = 0;
+	second = 0;
+}
+Goods::Goods(double f, int s) throw(My_Error_Range)
+{
+	if ((f <= 0) || (s <= 0))
+		throw new My_Error_Range("\n\nНеправильно введені дані");
+	first = f;
+	second = s;
+}
+Goods::Goods(const Goods& R)
+{
+	first = R.first;
+	second = R.second;
+}
+void Goods::Setfirst(double f)
+{
+	first = f;
+}
+void Goods::Setsecond(int s)
+{
+	second = s;
+}
+double Goods::Getfirst() const
+{
+	return first;
+}
+int Goods::Getsecond() const
+{
+	return second;
+}
+
+Goods& Goods::operator = (const Goods& R)
+{
+	first = R.first;
+	second = R.second;
+
+	return *this;
+}
+Goods::operator string() const
+{
+	stringstream sout;
+	sout << "\nПеретворення в літерний рядок:";
+	sout << "\nfirst" << first;
+	sout << "\nsecond" << second;
+
+	return sout.str();
+}
+ostream& operator <<(ostream& out, const Goods& R)
+{
+	out << "\n\nВведені вами значення:";
+	out << "\nfirst = " << R.first;
+	out << "\nsecond = " << R.second;
+
+	return out;
+}
+istream& operator >>(istream& in, Goods& R) throw(std::out_of_range)
+{
+	double first_1;
+	int second_2;
+
+	cout << "\nВведіть значення аргументів:";
+	cout << "\nfirst = "; in >> first_1;
+	cout << "\nsecond = ";in >> second_2;
+
+	if ((first_1 <= 0) || (second_2 <= 0))
+		throw std::out_of_range{ "\n\nДані введені неправильно" };
+
+	R.first = first_1;
+	R.second = second_2;
+
+	return in;
+
+}
+Goods& Goods::operator ++()
+{
+	++first;
+	return *this;
+}
+Goods& Goods::operator --()
+{
+	--first;
+	return *this;
+}
+Goods Goods::operator++(int)
+{
+	Goods a(*this);
+	second++;
+	return a;
+}
+Goods Goods::operator --(int)
+{
+	Goods a(*this);
+	second--;;
+	return a;
+}
+double Goods::Cost()
+{
+	double F = first * second;
+	cout << "Ціна загалом = " << F << endl;
+	return F;
+}
+
---
+++ OOPLab 5.1A.cpp
@@ -0,0 +1,89 @@
+﻿#include <iostream>
+
+#include "Goods.h"
+#include "Windows.h"
+#include "Me_Error_size_memorry.h"
+#include "My_Error_Range.h"
+
+void test() throw(std::bad_exception);
+void my_unexp();
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	try {
+		Goods a(5.2, 4);
+		cout << a;
+
+		++a;
+		cout << a;
+		--a;
+		cout << a;
+		a++;
+		cout << a;
+		a--;
+		cout << a;
+
+		cout << a.operator string();
+	}
+	catch (My_Error_Range* a) {
+		std::cout << a->What();
+	}
+
+	try {
+		Goods b(17.1, 2);
+
+		cin >> b;
+		cout << b;
+
+		++b;
+		cout << b;
+		--b;
+		cout << b;
+		b++;
+		cout << b;
+		b--;
+		cout << b;
+
+		cout << b.operator string();
+		cout << endl;
+		b.Cost();
+	}
+	catch (std::out_of_range obj_1) {
+		std::cout << obj_1.what();
+	}
+
+	try {
+		int size(0);
+		std::cout << "\n\nВведіть розмір масива: ";  std::cin >> size;
+
+		if ((size < 0) || (size > 80'000'000))
+			throw Me_Error_size_memorry("\nНевірно виділена пам’ять");		//генерування об'єкта винятка
+		Goods* c = new Goods[size];
+	}
+	catch (Me_Error_size_memorry& obj_2) {		// зловлює помилку "Me_Error_size_memorry"
+		std::cout << obj_2.What();
+	}
+
+	set_unexpected(my_unexp);
+
+	try {	//захищений(контрольований) блок
+		test();
+	}
+	catch (const std::bad_exception& e)		//зловлює помилку "std::bad_exception"
+	{
+		std::cerr << "\nCaught " << e.what() << '\n';
+	}
+	return 0;
+}
+
+void test() throw(std::bad_exception)
+{
+	throw std::bad_exception{};
+}
+
+void my_unexp() {
+	throw;
+}
